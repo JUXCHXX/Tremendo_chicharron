1ff3d1549419bfe460a766d7247c03b3c1bb66c4
JUXCHXX <jflorianamador@gmail.com>
2026-08-28 09:25:15 -0500

    Analiticas vercelç

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,6 +9,7 @@ import {
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
@@ -136,6 +137,7 @@ function RootComponent() {
         <Outlet />
       </div>
       <SpeedInsights />
+      <Analytics />
     </QueryClientProvider>
   );
 }
